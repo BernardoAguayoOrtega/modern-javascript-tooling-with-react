0e7ec9cc8ac33bb24bbc503a31acb7478cca098b
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-22 13:41:45 -0500

    Use whatch mode as script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Im the app</h1>, document.getElementById('app'));
+ReactDOM.render(<h1>Im the app hey!!!!!!</h1>, document.getElementById('app'));
